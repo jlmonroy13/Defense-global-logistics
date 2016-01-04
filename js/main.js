912ac11cb62c86b08d9731657a295402400f35bf
Jorge  Luis Monroy Herrera <jlmonroy13@MacBook-Pro-de-Jorge.local>
2016-01-03 22:41:21 -0500

    empezando responsive

--- js/main.js
+++ js/main.js
@@ -3,3 +3,34 @@ $(document).ready(function(){
 	$('.bootstrap-switch-handle-on').text('EN');
 	$('.bootstrap-switch-handle-off').text('ES');
 });
+
+/*=======MENU  NAVBAR DOWN===================*/
+    $(".1li").click(function() {
+      $("html, body").animate({
+        scrollTop: $("#about-us").offset().top
+      }, "slow");
+    });
+
+    $(".2li").click(function() {
+      $("html, body").animate({
+        scrollTop: $("#our-services").offset().top
+      }, "slow");
+    });
+
+    $(".3li").click(function() {
+      $("html, body").animate({
+        scrollTop: $("#our-experience").offset().top
+      }, "slow");
+    });
+
+  $(".4li").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#contact-us").offset().top
+    }, "slow");
+  });
+
+  $(".ourservices").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#our-services").offset().top
+    }, "slow");
+  });
